dcc51455e03c25db0a1b4f0192216976a868cec8
Lee <leeirungu11313@gmail.com>
2024-03-20 14:05:49 +0300

    Add index,.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity) {
+  activity = activity || 'roller-skate';
+  return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(work){
+    work = work || `go to the office`;
+    return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(adj){
+    return function(str){
+        return `You are ${adj}${str}${adj}!`
+    }
+}
+wrapAdjective("*") ("a dedicated programer");
+wrapAdjective("||")("a dedicated programer");
